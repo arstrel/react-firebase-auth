d2eddcdd28ac9c89bc3833e02c1fc5d36baf61ac
Artem <48565841+arstrel@users.noreply.github.com>
2019-08-21 11:06:02 -0400

    Delete firebase.config.js

--- src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyBh5lQzqJQmo-9rY14Uxx82mKXbhB59j3k",
-  authDomain: "react-auth-a46eb.firebaseapp.com",
-  databaseURL: "https://react-auth-a46eb.firebaseio.com",
-  projectId: "react-auth-a46eb",
-  storageBucket: "",
-  messagingSenderId: "539130310918",
-  appId: "1:539130310918:web:6225dbe849dd2895"
-};
